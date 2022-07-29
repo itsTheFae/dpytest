d9ee238cffa28fd7bcee805bfd131ccd897acd4c
thefae <itsthefae@gmail.com>
2022-07-29 10:57:55 -0700

    Align edit test with 2.x returned messages as in-place edits are no more.

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -8,7 +8,8 @@ async def test_edit(bot):
     guild = bot.guilds[0]
     channel = guild.channels[0]
 
-    mes = await channel.send("Test Message")
-    await mes.edit(content="New Message")
+    mes_o = await channel.send("Test Message")
+    mes_e = await mes_o.edit(content="New Message")
 
-    assert mes.content == "New Message"
+    assert mes_o.content == "Test Message"
+    assert mes_e.content == "New Message"
